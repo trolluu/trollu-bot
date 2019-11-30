105d5db23fe94605b2ae818a823772faae4f5694
trolluu <53543839+trolluu@users.noreply.github.com>
2019-11-30 23:42:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -22,29 +22,15 @@ client.categories = fs.readdirSync("./commands/");
 });
 
 client.on("ready", () => {
-module.exports = bot => {
     console.log("Online!");
-
-    let statuses = [
-        `${bot.guilds.size}!`,
-        `over ${bot.users.size} users!`
-    ]
-    
-    setInterval(function() {
-        let status = statuses[Math.floor(Math.random() * statuses.length)];
-        bot.user.setActivity(status, {type: "WATCHING"});
-    
-    }, 5000)
-
-
-    //client.user.setPresence({
-        //status: "online",
-        //game: {
-            //name: "KUPI KNUT",
-            //type: "WATCHING"
-        //}
-    //});
-}});
+    client.user.setPresence({
+        status: "online",
+        game: {
+            name: "...",
+            type: "WATCHING"
+        }
+    });
+});
 
 client.on("message", async message => {
 
