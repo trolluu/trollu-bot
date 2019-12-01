ee58d0987dc2d98a6c7ad2d28709f5f1205e0f3a
trolluu <53543839+trolluu@users.noreply.github.com>
2019-12-01 05:55:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ if(message.author.bot)
                 .then(reaction => reaction.message.react('650553553663950848'))
                 .then(reaction => reaction.message.react('650553843725369355'))
                 .then(reaction => reaction.message.react('650559284685307935'))
-                .then(reaction => reaction.message.delete(5000))
+                .then(reaction => reaction.message.delete(20000))
                 .then(msg => console.log("Deleted message"))
                 .catch(err => console.error);
             }
@@ -80,7 +80,7 @@ if(message.author.bot)
         embed.setDescription("<:monkey:650553517638942722> - Małpa\n" +
         "<:frog:650553553663950848> - Żaba\n" +
         "<:poop:650553843725369355> - Kupa\n" +
-        "<:ok_hand:650559284685307935> - Ok");
+        "<:ok_hand:650559284685307935> - Ok\n");
 
 
 
