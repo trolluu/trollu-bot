1225505d960ac7e44cf84ebd39286f1d88f3cf16
trolluu <53543839+trolluu@users.noreply.github.com>
2019-12-01 21:56:21 +0100

    Update react.js

--- commands/other/react.js
+++ commands/other/react.js
@@ -10,36 +10,3 @@ module.exports = {
         message.react('🐒');
     }
 }
-
-// client.on('message', message => {
-//     if(message.author.bot)
-//     {
-//         if(message.embeds)
-//         {
-//             const embedMsg = message.embeds.find(msg => msg.title === 'Server Roles');
-//             if(embedMsg)
-//             {
-
-//                 messgae.react('')
-//                 .then(reaction => reaction.message.react(''))
-//                 .then(reaction => reaction.message.react(''))
-//                 .then(reaction => reaction.message.react(''))
-//                 .then(reaction => reaction.message.delete(5000))
-//                 .then(msg => console.log("Deleted message"))
-//                 .catch(err => console.error);
-//             }
-//         }
-//         return;
-//     }
-//     if(message.content.toLoweCase() === 'treact')
-//     {
-//         const embed = new RichEmbed();
-//         embed.setTitle("Server Roles");
-//         embed.setColor("RANDOM");
-//         embed.setDescription("<:monkey:650553517638942722>")
-
-
-
-//         message.channel.send(embed);
-//     }    
-// });    
