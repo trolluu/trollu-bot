4d9f1db2670d7c965171eba3c40ba6feaca4ae78
trolluu <53543839+trolluu@users.noreply.github.com>
2019-12-01 23:55:02 +0100

    Update index.js

--- index.js
+++ index.js
@@ -57,7 +57,8 @@ client.on("message", async message => {
     if (cmd.length === 0) return;
 
     let command = client.commands.get(cmd);
-    if (!command) command = client.commands.get(client.aliases.get(cmd));
+    if (!command) command = client.commands.get(client.aliases.get(cmd)).then(m => m.delete(5000));
+    
 
     if (command)
         command.run(client, message, args);
