8eefd8604bccde7eaf96cf519ff5f6db6c27e392
trolluu <53543839+trolluu@users.noreply.github.com>
2019-12-02 22:16:27 +0100

    Create poll.js

---
+++ commands/other/poll.js
@@ -0,0 +1,27 @@
+const { RichEmbed } = require("discord.js");
+
+module.exports = {
+    name: "poll",
+    category: "other",
+    description: "",
+    usage: "<poll question>",
+    run: async (bot, message, args) => {
+        //if(!message.member.roles.find(r => r.name === 'roleName')) return message.channel.send('This requires the role: roleName!');
+        if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('This requires the permission: ADMINISTRATOR!');
+        
+        if(!args[0]) return message.channel.send('Proper Usage: <prefix>poll question');
+
+        const embed = new RichEmbed()
+            .setColor(0xffffff)
+            .setFooter('React to vote.')
+            .setDescription(args.join(' '))
+            .setTitle(`Poll Created By ${message.author.username}`);
+
+        let msg = await message.channel.send(embed);
+
+        await msg.react('✅');
+        await msg.react('❌');
+
+        message.delete({timeout: 1000});
+    }
+}
