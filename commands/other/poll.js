96406cec155ad26f27ea778e8032f6f6862e4cdf
trolluu <53543839+trolluu@users.noreply.github.com>
2019-12-02 22:18:20 +0100

    Update poll.js

--- commands/other/poll.js
+++ commands/other/poll.js
@@ -6,10 +6,10 @@ module.exports = {
     description: "",
     usage: "<poll question>",
     run: async (bot, message, args) => {
-        //if(!message.member.roles.find(r => r.name === 'roleName')) return message.channel.send('This requires the role: roleName!');
-        if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('This requires the permission: ADMINISTRATOR!');
+        //if(!message.member.roles.find(r => r.name === 'roleName')) return message.channel.send('This requires the role: roleName!').then(m => m.delete(5000));
+        if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('This requires the permission: ADMINISTRATOR!').then(m => m.delete(5000));
         
-        if(!args[0]) return message.channel.send('Proper Usage: <prefix>poll question');
+        if(!args[0]) return message.channel.send('Proper Usage: <prefix>poll question').then(m => m.delete(5000));
 
         const embed = new RichEmbed()
             .setColor(0xffffff)
