b4284b81807eda27ba61d0e179218284ca94b9ad
trolluu <53543839+trolluu@users.noreply.github.com>
2019-12-02 21:59:31 +0100

    Update calculator.js

--- commands/other/calculator.js
+++ commands/other/calculator.js
@@ -7,20 +7,20 @@ module.exports = {
     category: "other",
     description: "",
     run: async (client, message, args, tools) => {
-        if(!args[0]) return message.channel.send('Please input a calculation!');
+        if(!args[0]) return message.channel.send('Please input a calculation!').then(m => m.delete(5000));
 
         let resp;
         try {
             resp = math.eval(args.join(' '));
         }catch (e) {
-            return message.channel.send('Sorry, please input a valid calculation!');
+            return message.channel.send('Sorry, please input a valid calculation!').then(m => m.delete(5000));
         }
 
         const embed = new RichEmbed()
             .setColor(0xffffff)
             .setTitle('Calculator')
-            .addField('Input', `\`\`\`js\n${args.join('')}\`\`\``)
-            .addField('Output',`\`\`\`js\n${resp}\`\`\``)
+            .addField('**Input**', `\`\`\`js\n${args.join('')}\`\`\``)
+            .addField('**Output**',`\`\`\`js\n${resp}\`\`\``)
 
         message.channel.send(embed);
     }
