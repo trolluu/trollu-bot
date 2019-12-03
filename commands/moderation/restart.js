e1947e73526d873522ae915c3825906421de54c7
trolluu <53543839+trolluu@users.noreply.github.com>
2019-12-03 21:27:53 +0100

    Create restart.js

---
+++ commands/moderation/restart.js
@@ -0,0 +1,20 @@
+const Discord = require("discord.js")
+
+
+
+module.exports = {
+    name: "restart",
+    description: "Bot restart",
+    category: "moderation",
+    run: async (bot, message, args) => {
+        if(message.author.id != "323113623721607168") return message.channel.send("You're the bot the owner!")
+        
+        try {
+            await message.channel.send("Bot are restarting...")
+            process.exit()
+        }  catch(e) {
+            message.channel.send(`ERROR: ${e.message}`)
+        }
+    
+    }
+}    
