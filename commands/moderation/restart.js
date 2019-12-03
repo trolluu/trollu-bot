ff0fec5582a88ed114464c609eef9dd3263049aa
trolluu <53543839+trolluu@users.noreply.github.com>
2019-12-03 21:29:48 +0100

    Update restart.js

--- commands/moderation/restart.js
+++ commands/moderation/restart.js
@@ -7,7 +7,7 @@ module.exports = {
     description: "Bot restart",
     category: "moderation",
     run: async (bot, message, args) => {
-        if(message.author.id != "323113623721607168") return message.channel.send("You're the bot the owner!")
+        if(message.author.id != "323113623721607168") return message.channel.send("You're not the bot owner!")
         
         try {
             await message.channel.send("Bot are restarting...")
