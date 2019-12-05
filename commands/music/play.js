671718d68d20a2359fce8cf63d8fdf87bb69c511
trolluu <53543839+trolluu@users.noreply.github.com>
2019-12-05 19:33:02 +0100

    Create play.js

---
+++ commands/music/play.js
@@ -0,0 +1,24 @@
+const ytdl = require("ytdl-core");
+
+
+module.exports = {
+    name: "play",
+    aliases: ["p"],
+    category: "music",
+    description: "",
+    run: async (client, message, args, ops) => {
+        if(!message.member.voiceChannel) return message.channel.send("Please connect to a voice channel.");
+        if(message.guild.me.voiceChannel) return message.channel.send("Sorry, the bot is already connected to the guild.");
+        if(!args[0]) return message.channel.send("Sorry, please input a url following the command.");
+
+        let validate = await ytdl.validateURL(args[0]);
+
+        if(!validate) return message.channel.send("Sorry, please input a **valid** url following the command.");
+
+        let info = await ytdl.getInfo(args[0]);
+        let connection = await message.member.voiceChannel.join();
+        let discpatcher = await connection.play(ytdl(args[0], {filter: 'audioonly' }));
+
+        message.channel.send(`Now playing, ${info.title}`);
+    }
+}    
