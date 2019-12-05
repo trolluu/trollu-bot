90dfdfee8253479a08ddaa5d4baf2d4868d0d07d
trolluu <53543839+trolluu@users.noreply.github.com>
2019-12-05 22:37:33 +0100

    Create weather.js

---
+++ commands/info/weather.js
@@ -0,0 +1,35 @@
+const { RichEmbed } = require("discord.js");
+const weather = require("weather-js");
+
+module.exports = {
+    name: "weather",
+    category: "info",
+    description: "",
+    run: async (client, message, args) => {
+
+    weather.find({search: args.join(" "), deagreeType: 'C'}, function(err, result) {
+        if(err) message.channel.send(err);
+
+        if(result.length === 0) {
+            message.channel.send('**Please enter a valid location.**')
+        }
+    
+        var current = result[0].current;
+        var location = result[0].location;
+
+        const embed = new RichEmbed()
+        .setDescription(`**${current.skytext}**`)
+        .setAuthor(`Weather for ${current.observationpoint}`)
+        .setThumbnail(current.imageURL)
+        .setColor(0x00AF86)
+        .addField('Timezone', `GMT${location.timezone}`, true)
+        .addField('Degree Type',location.deagreeType, true)
+        .addField('Temperature',`${current.temperature}`, true)
+        .addField('Feels Like', `${current.feelslike}`, true)
+        .addField('Winds',current.winddisplay, true)
+        .addField('Humidity', `${current.humidity}%`, true)
+
+        message.channel.send({embed});
+    })
+    }
+}    
