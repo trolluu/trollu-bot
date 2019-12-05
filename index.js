07f8e6ade7c1d7df3a6a2a76e26048303d79d5c6
trolluu <53543839+trolluu@users.noreply.github.com>
2019-12-05 20:54:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -87,7 +87,7 @@ client.on("message", async message => {
             active: active
         }
     
-        let commandFile = require(`./commands/${cmd}.js`);
+        //let commandFile = require(`./commands/${cmd}.js`);
         commandFile.run(client, message, args, ops);
     }catch(e) {
         console.log(e);
