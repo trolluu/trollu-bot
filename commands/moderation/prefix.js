9992223d8bf79ba1c88347ff544ba9eaa521c2ab
trolluu <53543839+trolluu@users.noreply.github.com>
2019-12-07 02:15:33 +0100

    Update prefix.js

--- commands/moderation/prefix.js
+++ commands/moderation/prefix.js
@@ -10,13 +10,13 @@ module.exports = {
         if(!message.member.hasPermission("MANAGE_SERVER")) return message.reply("No no no.");
         if(!args[0] || args[0 == "help"]) return message.reply(`Usage: ${prefix}prefix <desired prefix here>`);
 
-        let prefixes = JSON.parse(fs.readFileSync("../../prefixes.json", "utf8"));
+        let prefixes = JSON.parse(fs.readFileSync("./prefixes.json", "utf8"));
 
         prefixes[message.guild.id] = {
             prefixes: args[0]
         };
     
-        fs.writeFile("../../prefixes.json", JSON.stringify(prefixes), (err) => {
+        fs.writeFile("./prefixes.json", JSON.stringify(prefixes), (err) => {
             if(err) console.log(err)
         });
 
