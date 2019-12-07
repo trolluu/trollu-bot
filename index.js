78831f0ff2e8434c88c4d8217135ca2671ff2c01
trolluu <53543839+trolluu@users.noreply.github.com>
2019-12-07 02:15:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ client.on("ready", () => {
     client.user.setPresence({
         status: "idle",
         game: {
-            name: "⚠ PRZERWA TECHNICZNA ⚠",
+            name: "⛔ PRZERWA TECHNICZNA ⛔",
             type: "WATCHING"
         }
     });
@@ -56,7 +56,7 @@ client.on("message", async message => {
     if (!message.guild) return;
     //if (!message.content.startsWith(prefix)) return;
     
-    let prefixes = JSON.parse(fs.readFileSync("../../prefixes.json", "utf8"));
+    let prefixes = JSON.parse(fs.readFileSync("./prefixes.json", "utf8"));
 
     if(!prefixes[message.guild.id]) {
         prefixes[message.guild.id] = {
