ffa3183a76902673ec397f9933ec8cf45b763ff3
trolluu <53543839+trolluu@users.noreply.github.com>
2019-12-07 01:47:35 +0100

    Create prefix.js

---
+++ commands/moderation/prefix.js
@@ -0,0 +1,30 @@
+const { RichEmbed } = require("discord.js");
+const fs = require("fs");
+
+module.exports = {
+    name: "prefix",
+    description: "Calculate the love affinity you have for another person.",
+    category: "moderation",
+    usage: "[mention | id | username]",
+    run: async (bot, message, args) => {
+        if(!message.member.hasPermission("MANAGE_SERVER")) return message.reply("No no no.");
+        if(!args[0] || args[0 == "help"]) return message.reply(`Usage: ${prefix}prefix <desired prefix here>`);
+
+        let prefixes = JSON.parse(fs.readFileSync("./prefixes.json", "utf8"));
+
+        prefixes[message.guild.id] = {
+            prefixes: args[0]
+        };
+    
+        fs.writeFile("./prefixes,json", JSON.stringify(prefixes), (err) => {
+            if(err) console.log(err)
+        });
+
+        const embed = new RichEmbed()
+        .setColor("FF9900")
+        .setTitle("Prefix Set!")
+        .setDescription(`Set to ${args[0]}`);
+
+        message.channel.send(embed);
+    }
+}    
