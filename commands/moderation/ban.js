fd72315e4e650381987a566e3903b7f86cdb0692
trolluu <53543839+trolluu@users.noreply.github.com>
2019-12-08 23:03:39 +0100

    Update ban.js

--- commands/moderation/ban.js
+++ commands/moderation/ban.js
@@ -5,7 +5,7 @@ const { promptMessage } = require("../../functions.js");
 module.exports = {
     name: "ban",
     category: "moderation",
-    description: "bans the member",
+    description: "Bans the member.",
     usage: "<id | mention>",
     run: async (client, message, args) => {
         const logChannel = message.guild.channels.find(c => c.name === "logs") || message.channel;
