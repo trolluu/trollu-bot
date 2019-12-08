e9de9dd0615ce6171b4c3d8247bea9f49c4c42a4
trolluu <53543839+trolluu@users.noreply.github.com>
2019-12-08 22:29:45 +0100

    Create avatar.js

---
+++ commands/other/avatar.js
@@ -0,0 +1,10 @@
+module.exports = {
+    name: "avatar",
+    description: "Sends the user's avatar",
+    category: "other",
+    usage: "<name>",
+    run: async (bot, message, args) => {
+        let aTaged = message.mentions.users.first();
+        message.channel.send(`Avatar ${aTaged.display.tag} ${aTaged.displayAvatarURL}`);
+    }
+}
