cb7e8e3c7cc16be6d5646c74b9bcd53a5b5458cf
trolluu <53543839+trolluu@users.noreply.github.com>
2019-12-08 23:03:51 +0100

    Update meme.js

--- commands/fun/meme.js
+++ commands/fun/meme.js
@@ -4,7 +4,7 @@ const randomPuppy = require ("random-puppy");
 module.exports = {
     name: "meme",
     category: "fun",
-    description: "Sends an epic meme",
+    description: "Sends an epic meme.",
     run: async (client, message, args) => {
         const subReddits = ["dankmeme", "meme", "me_irl"];
         const random = subReddits[Math.floor(Math.random() * subReddits.length)];
