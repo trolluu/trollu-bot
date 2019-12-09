23436a9b6f8cbf848d165c27508d8f0a0591b4a2
trolluu <53543839+trolluu@users.noreply.github.com>
2019-12-09 23:34:21 +0100

    Update warnlevel.js

--- commands/moderation/warnlevel.js
+++ commands/moderation/warnlevel.js
@@ -1,7 +1,7 @@
 const { RichEmbed } = require("discord.js");
 const fs = require("fs");
 const ms = require("ms");
-let warns = JSON.parse(fs.readFileSync("./warnings.json", "utf8"));
+let warns = JSON.parse(fs.readFileSync("../../warnings.json", "utf8"));
 
 module.exports = {
     name: "warnlevel",
