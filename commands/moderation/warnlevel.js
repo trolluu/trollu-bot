61194a5ea17bed2b45f293620e6aa8eefbed5e30
trolluu <53543839+trolluu@users.noreply.github.com>
2019-12-09 23:20:32 +0100

    Create warnlevel.js

---
+++ commands/moderation/warnlevel.js
@@ -0,0 +1,20 @@
+const { RichEmbed } = require("discord.js");
+const fs = require("fs");
+const ms = require("ms");
+let warns = JSON.parse(fs.readFileSync("./warnings.json", "utf8"));
+
+module.exports = {
+    name: "warnlevel",
+    aliases: ["wl"],
+    category: "moderation",
+    description: "Check the warn level.",
+    usage: "<id>",
+    run: async (bot, message, args) => {
+        if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.reply("You can't do that.");
+        let wUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
+        if(!wUser) return message.reply("Couldn't find them yo");
+        let warnlevel = warns[wUser.id].warns;
+
+        message.reply(`<@${wUser.id}> has ${warnlevel} warnings.`)
+    }
+}
