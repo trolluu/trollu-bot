2cbc221033fd9da257d230fd323108c8f3dcd406
trolluu <53543839+trolluu@users.noreply.github.com>
2019-12-09 22:00:35 +0100

    Update uptime.js

--- commands/info/uptime.js
+++ commands/info/uptime.js
@@ -15,18 +15,8 @@ module.exports = {
             const hrs = Math.floor((ms / (1000 * 60 * 60)) % 60).toString()
             const days = Math.floor((ms / (1000 * 60 * 60 * 24)) % 60).toString()
             return `${days.padStart(1, '0')} days, ${hrs.padStart(2, '0')} hours, ${min.padStart(2, '0')} minutes, ${sec.padStart(2, '0')} seconds!`
-        
-            .setColor("RANDOM")
-            .setThumbnail(message.guild.displayAvatarURL)
-            .setFooter(message.guild.me.displayName, client.user.dispalyAvatarURL)
-            .addField("I have been online for:")
-            .addField(`${duration(bot.uptime)}`)
-            .setTimestamp()
-
-            message.channel.send(embed);
         }
-        
 
-        //message.channel.send(`I have been online for: ${duration(bot.uptime)}`).then(m => m.delete(15000))
+        message.channel.send(`**I have been online for:** ${duration(bot.uptime)}`).then(m => m.delete(15000))
     }
 }
