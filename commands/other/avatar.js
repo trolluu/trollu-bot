ec734c264e1a2d47c3613b996ea3b47c87e6be6a
trolluu <53543839+trolluu@users.noreply.github.com>
2019-12-11 23:05:42 +0100

    Update avatar.js

--- commands/other/avatar.js
+++ commands/other/avatar.js
@@ -1,3 +1,5 @@
+const { RichEmbed } = require("discord.js");
+
 module.exports = {
     name: "avatar",
     description: "Sends the user's avatar.",
@@ -5,7 +7,15 @@ module.exports = {
     usage: "<name>",
     run: async (bot, message, args) => {
         if (message.deletable) message.delete();
-        let aTaged = message.mentions.users.first();
-        message.channel.send(`**Avatar:** (${aTaged.displayAvatarURL})`);
+        // let aTaged = message.mentions.users.first();
+        // message.channel.send(`**Avatar:** (${aTaged.displayAvatarURL})`);
+
+        const embed = new RichEmbed()
+        .setColor("RANDOM")
+        .setTitle(`${user.tag}s profile picture!`)
+        .setURL(user.displayAvatarURL)
+        .setImage(user.displayAvatarURL)
+
+        message.channel.send(embed);
     }
 }
