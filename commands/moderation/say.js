2c01b74f91632024121512e96abbc886dd680945
trolluu <53543839+trolluu@users.noreply.github.com>
2019-12-11 23:29:53 +0100

    Create say.js

---
+++ commands/moderation/say.js
@@ -0,0 +1,37 @@
+const { RichEmbed } = require("discord.js");
+
+module.exports = {
+    name: "say",
+    aliases: ["bc", "broadcast"],
+    category: "moderation",
+    description: "Says your input via the bot.",
+    usage: "<input>",
+    run: async (client, message, args) => {
+        if (message.deletable) message.delete();
+
+        if (!message.member.hasPermission("ADMINISTRATOR")) {
+            return message.reply("❌ You do not have permissions to commmand **say**!")
+                .then(m => m.delete(5000));
+        
+        }
+
+        if (args.length < 1) 
+            return message.reply("Nothing to say?").then(m => m.delete(5000));
+    
+        const roleColor = message.guild.me.displayHexColor === "#000000" ? "#ffffff" : message.guild.me.displayHexColor;
+
+        if (args[0].toLowerCase() === "embed") {
+            const embed = new RichEmbed()
+                .setColor(roleColor)
+                .setDescription(args.slice(1).join(" "))
+                .setTimestamp()
+                .setImage(client.user.displayAvatarURL)
+                .setAuthor(message.author.username, message.author.displayAvatarURL)
+                .setFooter(client.user.username, client.user.displayAvatarURL)
+
+            message.channel.send(embed);
+        }else {
+            message.channel.send(args.join(" "));   
+        }
+    }
+}
